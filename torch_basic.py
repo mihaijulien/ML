ed5977e65f400f33a2e303a73290e1c9b300e49b
Julien Mihai <mihaijulien@gmail.com>
2021-01-11 21:25:50 +0200

    Linear regression torch

--- torch_basic.py
+++ torch_basic.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 
+'''
 x = torch.rand(5, 3)
 print(x)
 
@@ -15,3 +16,32 @@ print(x)
 
 x = torch.ones(5)
 print(x)
+'''
+
+# LR
+
+'''
+w = torch.tensor(3.0, requires_grad=True)
+b = torch.tensor(1.0, requires_grad=True)
+
+def forward(x):
+	y = w *x +b
+	return y
+
+x = torch.tensor(2)
+print(forward(x))
+'''
+
+from torch.nn import Linear
+torch.manual_seed(1)
+
+
+model = Linear(in_features=1, out_features=1)
+print(model.bias, model.weight)
+
+x = torch.tensor([2.0])
+print(model(x))
+
+
+
+
