52cdf9f25db45999f027ce8c5265f2e49164602f
Julien Mihai <mihaijulien@gmail.com>
2021-03-23 11:12:30 +0200

    One hot encoding for target variable, use tree classifier instead or LR

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,13 +2,13 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
 # create dataset
 fizzbuzz = {}
 
-for i in range(1, 1000):
+for i in range(101, 1000):
 	if(i%3 == 0 and i%5 == 0):
 		fizzbuzz[i] = 'fizzbuzz'
 	elif(i%3 == 0):
@@ -20,13 +20,16 @@ for i in range(1, 1000):
 
 data = {'Number': list(fizzbuzz.keys()), 'FizzBuzz': list(fizzbuzz.values())}
 df = pd.DataFrame(data)
-
-
-df['FizzBuzz'].replace(['fizz', 'buzz', 'fizzbuzz'], [3,5,35], inplace=True)
 print(df.head())
 
+def fizzbuzz_encode(i):
+	if   (i == 'fizzbuzz') : return np.array([0,0,0,1])
+	elif (i == 'fizz')     : return np.array([0,0,1,0])
+	elif (i == 'buzz')     : return np.array([0,1,0,0])
+	else                 : return np.array([1,0,0,0])
+
 X = df['Number'].values
-y = df['FizzBuzz'].values
+y = np.array([fizzbuzz_encode(i) for i in df['FizzBuzz'].values])
 X = StandardScaler().fit_transform(X.reshape(-1, 1))
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
@@ -34,11 +37,29 @@ X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
 print(X_train.shape)
 print(y_train.shape)
 
-model = LinearRegression()
+model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
 
 pred = model.predict(X_test)
 
 print('Train set accuracy: ', accuracy_score(y_train, model.predict(X_train)))
 print('Test set accuracy: ', accuracy_score(y_test, pred))
-#print("MSE: ", np.mean(((y_test - prediction) ** 2)))
+
+# test model with new data
+def predict_number(n):
+	pred = model.predict(np.array([n]).reshape(1,-1))
+	
+	if (pred[0][0] == 1.0) : 
+		print('fizzbuzz')
+		return 'fizzbuzz'
+	elif (pred[0][1] == 1.0) : 
+		return 'fizz'
+	elif (pred[0][2] == 1.0) :
+		return 'buzz'
+	else: 
+		return n
+
+print('1', predict_number(1))
+print('3', predict_number(3))
+print('5', predict_number(5))
+print('15', predict_number(15))
